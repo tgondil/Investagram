247caf6243b0a1d41e0f971f2159ee44eb793119
ElliotSmith5 <90064250+ElliotSmith5@users.noreply.github.com>
2024-02-25 17:13:52 -0500

    Create Auth.js
    Implemented user authentication to ensure only authorized users can create, edit, view, or delete posts.

---
+++ Auth.js
@@ -0,0 +1,59 @@
+// auth.js
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const bcrypt = require('bcryptjs');
+const { User } = require('./models');
+
+// Local Strategy (for login)
+passport.use(
+  new LocalStrategy(
+    { usernameField: 'email', passwordField: 'password' },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          return done(null, false, { message: 'Incorrect email.' });
+        }
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+// JWT Strategy
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'tanay-is-a-tank', //
+    },
+    async (jwt_payload, done) => {
+      try {
+        const user = await User.findById(jwt_payload.id);
+
+        if (!user) {
+          return done(null, false);
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error, false);
+      }
+    }
+  )
+);
+
+module.exports = passport;
