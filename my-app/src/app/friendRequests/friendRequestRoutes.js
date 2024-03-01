a1bc6afd00225f9c4da47a9f505a3ee5b1282080
ElliotSmith5 <90064250+ElliotSmith5@users.noreply.github.com>
2024-03-01 02:54:40 -0500

    Update friendRequestRoutes.js

--- my-app/src/app/friendRequests/friendRequestRoutes.js
+++ my-app/src/app/friendRequests/friendRequestRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const friendRequestController = require('../controllers/friendRequestController');
+const friendRequestController = require('./usernamefriendRequestController');
 
 // Endpoint to send a friend request
 router.post('/send-request/:recipientId', friendRequestController.sendFriendRequest);
