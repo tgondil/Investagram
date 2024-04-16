fe5e02eb9b50d4832e4c2f41a55f4cb5e220de17
Jason Yang <97807901+h3yJ@users.noreply.github.com>
2024-04-16 01:37:27 -0400

    Homepage update
    Got the search bar and dropdown menu visually there. Still need backend code and functionality implemented.

--- my-app/src/app/home/page.jsx
+++ my-app/src/app/home/page.jsx
@@ -6,42 +6,42 @@ import Feed from "../../components/feed";
 import Cookies from 'js-cookie'
 
 export default function page() {
-  const [searchType, setSearchType] = useState("users"); // Default search type
+  const [searchType, setSearchType] = useState("posts"); // Default search type
   const [searchQuery, setSearchQuery] = useState(""); // Search query
 
   return (
-    <main className="h-screen bg-shark-950 w-full fixed">
+    <main className="h-screen bg-shark-950 overflow-hidden">
     <div className="flex">
       <Sidebar className="w-1/5" />
-      <div className="w-10/12 h-screen">
-        {/* Search bar container */}
-        <div className="border-b border-dotted fixed w-full h-28 bg-shark-950 flex items-center p-4">
-          {/* Search Input */}
-          <input
-            type="text"
-            placeholder="Search..."
-            className="h-10 px-5 pr-16 rounded-full text-sm focus:outline-none"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
+      <div className="w-4/5">
+          {/* Search bar container */}
+          <div className="border-b border-dotted fixed top-0 right-0 w-4/5 h-20 bg-shark-950 flex items-center justify-between p-5">
+            {/* Search Input */}
+            <input
+              type="text"
+              placeholder="Search..."
+              className="h-10 px-5 w-3/4 rounded-full text-sm focus:outline-none text-black"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            
+            {/* Dropdown Menu */}
+            <select
+              className="h-10 w-1/5 rounded-full text-sm text-black text-center"
+              value={searchType}
+              onChange={(e) => setSearchType(e.target.value)}
+            >
+              <option value="posts">Posts</option>
+              <option value="users">Users</option>
+            </select>
+          </div>
           
-          {/* Dropdown Menu */}
-          <select
-            className="ml-4 h-10 rounded-full text-sm"
-            value={searchType}
-            onChange={(e) => setSearchType(e.target.value)}
-          >
-            <option value="posts">Users</option>
-            <option value="users">Posts</option>
-          </select>
-        </div>
-        
-        {/* Push content down since the search bar is fixed */}
-        <div className="pt-28">
-          <Feed />
+          {/* Push content down since the search bar is fixed */}
+          <div className="pt-20">
+            <Feed />
+          </div>
         </div>
       </div>
-    </div>
-  </main>
-);
+    </main>
+  );
 }
